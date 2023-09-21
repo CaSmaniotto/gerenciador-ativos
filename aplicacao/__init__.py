cd66820658c5ccf9fed551b4c7c58bddc92dc0a6
Caetano Smaniotto <70862409+CaSmaniotto@users.noreply.github.com>
2023-09-21 09:29:59 -0300

    Update __init__.py

--- aplicacao/__init__.py
+++ aplicacao/__init__.py
@@ -8,11 +8,7 @@ from sendgrid import SendGridAPIClient
 
 # api key sendgrid
 # sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-# <<<<<<< HEAD
-# sg = SendGridAPIClient("api_key")
-# =======
 sg = SendGridAPIClient("YOUR_API_KEY")
-# >>>>>>> bcf1f3d5176ae4c560d59810182aa30d40c8e0e6
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///almoxarifado.db"
