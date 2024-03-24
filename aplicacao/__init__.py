8f283210dbe2f50d7bbe88948de0fdd3bac9def6
Caetano Smaniotto <smaniottocaetano@gmail.com>
2024-03-24 00:24:40 -0300

    initial

--- aplicacao/__init__.py
+++ aplicacao/__init__.py
@@ -1,26 +1,14 @@
-from flask import Flask, session
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 from flask_admin import Admin
-from sendgrid import SendGridAPIClient
 
-# sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-sg = SendGridAPIClient("API_KEY")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///almoxarifado.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///barbearia.db"
 app.config["SECRET_KEY"] = "SECRET_KEY"
 
 database = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "index"
-login_manager.session_protected = None
-login_manager.login_message = "Por favor faça login para acessar essa página!"
-admin = Admin(app, name='Painel de Controle', template_mode='bootstrap4', url='/admin', endpoint='first')
 
-login_manager.login_view = 'index'
-login_manager.refresh_view = 'index'
+admin = Admin(app, name='Gerenciador', template_mode='bootstrap3')
 
 from aplicacao import routes
